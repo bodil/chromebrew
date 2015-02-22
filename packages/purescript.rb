ccc5067f0e8ea0eeb72735bf06d54d8528ab084f
Bodil Stokke <bodil@bodil.org>
2015-02-22 15:16:56 +0000

    Purescript 0.6.8.

--- packages/purescript.rb
+++ packages/purescript.rb
@@ -1,12 +1,12 @@
 require 'package'
 
 class Purescript < Package
-  version '0.6.4'
-  source_url 'https://github.com/purescript/purescript/releases/download/v0.6.4/linux64.tar.gz'
-  source_sha1 '805826a5e45aaddf6963c4adbb5a09a0ea3a99cb'
+  version '0.6.8'
+  source_url 'https://github.com/purescript/purescript/releases/download/v0.6.8/linux64.tar.gz'
+  source_sha1 '381998669b1aeb914f8987eea26934d026747f2e'
 
   depends_on 'ncurses_so'
-  
+
   def self.install
     system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
     system "install -m0755 psc* #{CREW_DEST_DIR}/usr/local/bin"
